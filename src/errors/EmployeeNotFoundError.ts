868ca785550c8b3c4dbac27e76f2f64e9279e8df
chudskii.semyon@gmail.com <semyon.chudskii@gmail.com>
2020-05-09 00:22:58 +0300

    add employee not found error

---
+++ src/errors/EmployeeNotFoundError.ts
@@ -0,0 +1,18 @@
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export const INVALID_EMAIL_OR_PASSWORD_ERROR =
+    'INVALID_EMAIL_OR_PASSWORD_ERROR';
+
+export class InvalidEmailOrPasswordError extends HttpException {
+    constructor(
+    ) {
+        super(
+            {
+                message: 'Invalid email or password',
+                status: INVALID_EMAIL_OR_PASSWORD_ERROR,
+            },
+            HttpStatus.BAD_REQUEST,
+        );
+
+    }
+}
