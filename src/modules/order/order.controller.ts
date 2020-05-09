450537e02047fa348ef7636a2dffba8508066bea
chudskii.semyon@gmail.com <semyon.chudskii@gmail.com>
2020-05-09 12:12:32 +0300

    add guards to order controller

--- src/modules/order/order.controller.ts
+++ src/modules/order/order.controller.ts
@@ -1,9 +1,16 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { CreateOrderDto } from './DTOs/create-order.dto';
 import { LoggerService } from '../../logger/logger.service';
 import { OrderService } from './order.service';
 import { Order } from 'src/entities/order.entity';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Roles } from '../auth/decorators/roles.decorator';
+import { EmployeeRoleEnum } from '../../enums/employee-role.enum';
+import { RolesGuard } from '../auth/guards/roles.guard';
 
+const { CASHIER } = EmployeeRoleEnum;
+
+@UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('order')
 export class OrderController {
     private readonly loggerContext = this.constructor.name;
@@ -15,12 +22,14 @@ export class OrderController {
     }
 
     @Post()
+    @Roles(CASHIER)
     public async createOrder(@Body() createOrderDto: CreateOrderDto): Promise<Order> {
         this.logger.log({
-            message: 'proceed createOrder',
-            createOrderDto,
-            method: 'createOrder',
-        }, this.loggerContext);
+                message: 'proceed createOrder',
+                createOrderDto,
+                method: 'createOrder',
+            }, this.loggerContext
+        );
 
         return await this.orderService.createOrder(createOrderDto);
     }
