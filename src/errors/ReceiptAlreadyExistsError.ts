8baf0b3d3e4268b0c9c204f5703cfd82d45a884e
chudskii.semyon@gmail.com <semyon.chudskii@gmail.com>
2020-05-10 12:08:55 +0300

    update error text

--- src/errors/ReceiptAlreadyExistsError.ts
+++ src/errors/ReceiptAlreadyExistsError.ts
@@ -1,7 +1,7 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 export const RECEIPT_ALREADY_EXISTS_ERROR =
-    'RECEIPT_ALREADY_EXISTS';
+    'RECEIPT_ALREADY_EXISTS_ERROR';
 
 export class ReceiptAlreadyExistsError extends HttpException {
     constructor(message: string) {
