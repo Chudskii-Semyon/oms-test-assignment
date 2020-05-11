4d0d0e3afcffef7de2368166ed7d962fd7324d03
chudskii.semyon@gmail.com <semyon.chudskii@gmail.com>
2020-05-11 03:08:26 +0300

    add swagger doc to receipt controller

--- src/modules/receipt/receipt.controller.ts
+++ src/modules/receipt/receipt.controller.ts
@@ -9,9 +9,17 @@ import { RolesGuard } from '../auth/guards/roles.guard';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { EmployeeRoleEnum } from '../../enums/employee-role.enum';
 import { Employee } from '../../entities/employee.entity';
+import {
+    ApiBearerAuth,
+    ApiOperation,
+    ApiResponse,
+    ApiTags,
+} from '@nestjs/swagger';
 
 const { CASHIER } = EmployeeRoleEnum;
 
+@ApiTags('Receipts')
+@ApiBearerAuth()
 @UseGuards(JwtAuthGuard, RolesGuard)
 @Controller('orders/:orderId/receipts')
 export class ReceiptController {
@@ -23,6 +31,14 @@ export class ReceiptController {
     ) {
     }
 
+    @ApiOperation({
+        description: 'Endpoint for order creation. Only employee with role CASHIER' +
+            ' and who created order with provided id have access to this endpoint',
+    })
+    @ApiResponse({
+        status: 403,
+        description: `May occur if employee's role is not CASHIER or order was not created by this employee`,
+    })
     @Post()
     @Roles(CASHIER)
     public async createReceipt(@Param() createReceiptInput: CreateReceiptDto, @Req() req: Request): Promise<Receipt> {
